09a295785dc5bda4f18fe5510b1abdba7b455f6a
ageera <age-era@outlook.com>
2019-01-24 14:00:38 +0800

    2019年1月24日

--- Helloworld.java
+++ Helloworld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 1
  */
@@ -5,5 +7,7 @@ public class Helloworld {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        Scanner.class = new Scanner(System.in);
+        System.out.println(in.nextLine());
     }
 }
